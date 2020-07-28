be58c7af1440e998b676b29950d35efe0c9b81e6
Martin Stenflo <stenflo@gmail.com>
2020-07-28 15:59:54 -0700

    Letter press event handler

--- script.js
+++ script.js
@@ -33,4 +33,40 @@ function displayWord() {
   }
 }
 
+function updateWrongLettersEl() {
+  console.log('Update Wrong')
+}
+
+function showNotification() {
+  notification.classList.add('show');
+
+  setTimeout(() => {
+    notification.classList.remove('show')
+  }, 2000);
+}
+
+window.addEventListener('keydown', e => {
+  if (e.keyCode >= 65 && e.keyCode <= 90) {
+    const letter = e.key;
+
+    if (selectedWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+
+        displayWord();
+      } else {
+        showNotification();
+      }
+    } else {
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+
+        updateWrongLettersEl();
+      } else {
+        showNotification();
+      }
+    }
+  }
+})
+
 displayWord();
